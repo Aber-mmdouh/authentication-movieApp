97298caaef3c21eea39fd1f69a12f6ff4af79610
abeer mamdouh <aber9185@gmail.com>
2026-06-22 13:01:27 +0300

    modify routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Movie } from '../movie/movie';
 import { authguardGuard } from '../guards/authguard-guard';
 
 export const routes: Routes = [
+    {path:'',component:Register},
   {path:'Register',component:Register},
   {path:'movie',component:Movie,canActivate:[authguardGuard]}
 ];
